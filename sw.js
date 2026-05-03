6f8a54c11d75bd276325f3ff14d52e7b3491c622
coshea321 <cathal@oilean.com>
2026-05-03 14:21:17 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'freshlist-v2';
+const CACHE = 'freshlist-v3';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
